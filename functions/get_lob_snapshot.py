9b1f3fe6be8b4eb46288b4c958337aa49e33a614
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2020-03-21 10:30:56 +0000

    Save snapshots to DynamoDB table

--- functions/get_lob_snapshot.py
+++ functions/get_lob_snapshot.py
@@ -2,65 +2,73 @@ import time
 import requests
 import json
 from decimal import Decimal
+import boto3
 
-pairs = {
+TABLE_NAME = "LOBDumps"
+
+pairs = [
     "BTC_ATOM",
-    # "BTC_BCHABC",
-    # "BTC_BCHSV",
-    # "BTC_DASH",
-    # "BTC_DOGE",
-    # "BTC_EOS",
-    # "BTC_ETC",
-    # "BTC_ETH",
-    # "BTC_LTC",
-    # "BTC_MANA",
-    # "BTC_STORJ",
-    # "BTC_STR",
-    # "BTC_STRAT",
-    # "BTC_TRX",
-    # "BTC_XEM",
-    # "BTC_XMR",
-    # "BTC_XRP",
-    # "BTC_ZEC",
+    "BTC_BCHABC",
+    "BTC_BCHSV",
+    "BTC_DASH",
+    "BTC_EOS",
+    "BTC_ETC",
+    "BTC_ETH",
+    "BTC_LTC",
+    "BTC_MANA",
+    "BTC_STORJ",
+    "BTC_STR",
+    "BTC_STRAT",
+    "BTC_TRX",
+    "BTC_XEM",
+    "BTC_XMR",
+    "BTC_XRP",
+    "BTC_ZEC",
 
-    # "ETH_ETC",
-    # "ETH_ZEC",
+    "ETH_ETC",
 
-    # "TRX_ETH",
-    # "TRX_XRP",
+    "TRX_ETH",
+    "TRX_XRP",
 
-    # "USDC_BTC",
-    # "USDC_ETH",
-    # "USDC_USDT",
-    # "USDC_ZEC",
+    "USDC_BTC",
+    "USDC_ETH",
+    "USDC_USDT",
 
-    # "USDT_BAT",
-    # "USDT_BTC",
-    # "USDT_DASH",
-    # "USDT_DOGE",
-    # "USDT_EOS",
-    # "USDT_ETC",
-    # "USDT_ETH",
-    # "USDT_GNT",
-    # "USDT_LSK",
-    # "USDT_LTC",
-    # "USDT_MANA",
-    # "USDT_NXT",
-    # "USDT_REP",
-    # "USDT_STR",
-    # "USDT_XMR",
-    # "USDT_XRP",
-    # "USDT_ZEC",
-    # "USDT_ZRX",
-}
+    "USDT_BTC",
+    "USDT_DASH",
+    "USDT_EOS",
+    "USDT_ETH",
+    "USDT_GNT",
+    "USDT_LTC",
+    "USDT_NXT",
+    "USDT_STR",
+    "USDT_XMR",
+    "USDT_XRP",
+    "USDT_ZEC",
+    "USDT_ZRX",
+]
+
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
+table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(event, context):
-    all_books_response = requests.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=all&depth=100')
-    all_books = json.loads(all_books_response.text)
-    #all_books = json.loads(all_books_response.text, parse_float=Decimal)
-    filtered = { key :value for key, value in all_books.items() if key in pairs }
+    current_timestamp = int(time.time())
+    response = requests.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=all&depth=100')
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(filtered)
-    }
+    if response.status_code == 200:
+        all_books = json.loads(response.text, parse_float=Decimal)
+        filtered = { key :value for key, value in all_books.items() if key in pairs }
+
+        for pair, book in filtered.items():
+            table.put_item(
+            Item = {
+                    'Pair': pair,
+                    'Timestamp': current_timestamp,
+                    'book': book
+                    }
+            )
+        return {"statusCode": 200, "body": "Success"}
+
+    else:
+        print(response)
+        return {"statusCode": response.status_code, "body": "Error"}
