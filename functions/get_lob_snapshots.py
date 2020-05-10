383280eda3a0c4f7957ed811229d71be1d56e348
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2020-05-10 14:07:57 +0100

    Add an alarm for missing data, more logging in GetLOBSnaphots

--- functions/get_lob_snapshots.py
+++ functions/get_lob_snapshots.py
@@ -9,10 +9,11 @@ dynamodb = boto3.resource('dynamodb')
 table_name = os.environ['TABLE_NAME']
 table = dynamodb.Table(table_name)
 
-def get_lob_snapshot():
+def get_lob_snapshot(request_id):
     # Get LOB snapshots for all pairs from Poloniex
     response = requests.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=all&depth=100')
     if response.status_code == 200:
+        print('LOB snapshot downloaded successfully for RequestId: ' + request_id)
         # Compress json and save in DynamoDB
         snapshot_gz = gzip.compress(response.text.encode('utf-8'))
         current_timestamp = int(time.time())
@@ -24,23 +25,31 @@ def get_lob_snapshot():
                 'TTL': current_timestamp + (60 * 60 * 72) #72 hours
             }
         )
+        print('LOB snapshot saved to DynamoDB for RequestId: ' + request_id)
     else:
+        print('LOB snapshot download error for RequestId: ' + request_id)
         print(response)
 
 def lambda_handler(event, context):
     # Start at a time divisible by 10 seconds
-    till_cosest_ten = 10 - int(time.time()) % 10
-    time.sleep(till_cosest_ten)
+    current_second = time.gmtime().tm_sec
+
+    print('tm_sec:')
+    print(time.gmtime().tm_sec)
+
+    if current_second > 10:
+        print(time.strftime('Lambda execution delayed start at %H:%M:%S for RequestId: ' + context.aws_request_id, time.gmtime()))
+    time.sleep(10 - current_second % 10)
 
     # Create 5 threads executed with 10 seconds delay each
     threads = []
     for i in range(10, 51, 10):
-        thread = threading.Timer(i, get_lob_snapshot)
+        thread = threading.Timer(i, get_lob_snapshot, [context.aws_request_id])
         thread.start()
         threads.append(thread)
 
     # Get one now before 5 threads above start
-    get_lob_snapshot()
+    get_lob_snapshot(context.aws_request_id)
 
     # Wait for all threads to finish
     for thread in threads:
